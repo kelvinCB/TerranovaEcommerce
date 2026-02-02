984d7f82f13e3d865f8f91711265f95653870fe0
Kelvin Calcano <kelvinr02@hotmail.com>
2026-02-01 21:31:56 -0400

    [TE-002] chore: remove React Query Devtools from main entry point

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,5 +1,4 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import AppLayout from "@/components/ui/AppLayout";
@@ -41,7 +40,6 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
