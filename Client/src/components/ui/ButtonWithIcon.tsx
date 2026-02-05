468afacf383b167fcf20c3b5561728554ad07f37
Kelvin Calcano <kelvinr02@hotmail.com>
2026-02-05 09:33:30 -0400

    [TE-008] refactor(client): make ButtonWithIcon a styled Link

--- Client/src/components/ui/ButtonWithIcon.tsx
+++ Client/src/components/ui/ButtonWithIcon.tsx
@@ -1,10 +1,10 @@
-import { cloneElement, HTMLAttributes, ReactElement } from "react";
-import { Link } from "react-router-dom";
+import { cloneElement, ReactElement } from "react";
+import { Link, LinkProps } from "react-router-dom";
 
-interface ButtonWithIconProps extends HTMLAttributes<HTMLButtonElement> {
+interface ButtonWithIconProps extends Omit<LinkProps, "to"> {
   icon: ReactElement<{ size?: number; className?: string }>;
   size?: number;
-  to: string;
+  to: LinkProps["to"];
 }
 
 function ButtonWithIcon({
@@ -13,18 +13,19 @@ function ButtonWithIcon({
   className,
   to,
   children,
+  ...props
 }: ButtonWithIconProps) {
   return (
-    <Link to={to}>
-      <button
-        className={
-          (className ? className + " " : "") +
-          "bg-brand-500 hover:bg-brand-700 hover:text-brand-100 text-brand-50 flex cursor-pointer items-center gap-3 rounded-[22rem] py-1 pr-1 pl-6 text-xl font-medium"
-        }
-      >
-        {children}
-        {cloneElement(icon, { size })}
-      </button>
+    <Link
+      to={to}
+      className={
+        (className ? className + " " : "") +
+        "bg-brand-500 hover:bg-brand-700 hover:text-brand-100 text-brand-50 flex cursor-pointer items-center gap-3 rounded-[22rem] py-1 pr-1 pl-6 text-xl font-medium"
+      }
+      {...props}
+    >
+      {children}
+      {cloneElement(icon, { size })}
     </Link>
   );
 }
