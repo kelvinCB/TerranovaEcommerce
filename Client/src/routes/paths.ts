cfcc0fa0e6f74205f7625f1c02abd5825e16ff46
kelvinCB <kelvinCB@users.noreply.github.com>
2026-02-09 14:34:30 +0100

    [TE-014] feat: add contact path to route definitions

--- Client/src/routes/paths.ts
+++ Client/src/routes/paths.ts
@@ -4,6 +4,7 @@ export const PATHS = {
   address: "/address",
   cart: "/cart",
   wishlist: "/wishlist",
+  contact: "/contact",
   productsByCategory: "/products/:category",
   productDetails: "/products/:category/:id",
 } as const;
