bf4482de0be636f8b8cd40784f6d407c263efe2e
briangel29 <briangel29@gmail.com>
2026-02-10 15:01:46 -0400

    chore: apply DIP for id and time in domain layer and domain test layer

--- Backend-dotnet/test/Domain.Tests/Entities/CartTest.cs
+++ Backend-dotnet/test/Domain.Tests/Entities/CartTest.cs
@@ -8,43 +8,63 @@ namespace Domain.Test.Entities
         public void CreateCart_ShouldCreateCartWithValidUserId()
         {
             // Arrange
+            var id = Ulid.NewUlid();
             var userId = Ulid.NewUlid();
+            var utcNow = DateTimeOffset.UtcNow;
 
             // Act
-            var cart = new Cart(userId);
+            var cart = new Cart(id, userId, utcNow);
 
             // Assert
             Assert.NotNull(cart);
+            Assert.Equal(id, cart.Id);
             Assert.Equal(userId, cart.UserId);
             Assert.NotEqual(Ulid.Empty, cart.Id);
-            Assert.True(cart.CreateAt <= DateTimeOffset.UtcNow);
-            Assert.True(cart.UpdateAt <= DateTimeOffset.UtcNow);
+            Assert.Equal(utcNow, cart.CreatedAt);
+            Assert.Equal(utcNow, cart.UpdatedAt);
+            Assert.True(cart.CreatedAt <= DateTimeOffset.UtcNow);
+            Assert.True(cart.UpdatedAt <= DateTimeOffset.UtcNow);
+        }
+
+        [Fact]
+        public void CreateCart_ShouldThrowException_WhenIdIsEmpty(){
+            // Arrange
+            var id = Ulid.Empty;
+            var userId = Ulid.NewUlid();
+            var utcNow = DateTimeOffset.UtcNow;
+
+            // Act and Assert
+            var exception = Assert.Throws<ArgumentException>(() => new Cart(id, userId, utcNow));
+            Assert.Contains("Id is required.", exception.Message);
         }
 
         [Fact]
         public void CreateCart_ShouldThrowException_WhenUserIdIsEmpty()
         {
             // Arrange
+            var id = Ulid.NewUlid();
             var userId = Ulid.Empty;
+            var utcNow = DateTimeOffset.UtcNow;
 
-            // Act & Assert
-            var exception = Assert.Throws<ArgumentException>(() => new Cart(userId));
-            Assert.Equal("User Id is required. (Parameter 'userId')", exception.Message);
+            // Act and Assert
+            var exception = Assert.Throws<ArgumentException>(() => new Cart(id, userId, utcNow));
+            Assert.Contains("User Id is required.", exception.Message);
         }
 
         [Fact]
         public void UpdateCart_ShouldUpdatePropertyUpdateAt()
         {
             // Arrange
+            var id = Ulid.NewUlid();
             var userId = Ulid.NewUlid();
-            var cart = new Cart(userId);
-            var originalUpdateAt = cart.UpdateAt;
+            var utcNow = DateTimeOffset.UtcNow;
+            var cart = new Cart(id, userId, utcNow);
+            var originalUpdateAt = cart.UpdatedAt;
 
             // Act
-            cart.UpdateCart();
-
+            cart.UpdateCart(utcNow.AddMinutes(1));
             // Assert
-            Assert.True(cart.UpdateAt > originalUpdateAt);
+            Assert.True(cart.UpdatedAt > originalUpdateAt);
         }
     }
 }
