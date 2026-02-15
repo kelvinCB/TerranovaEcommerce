3f30a513a356288337f91b12a1fe6f846720c150
Briangel Abreu <briangel29@gmail.com>
2026-02-15 16:13:21 -0400

    chore: refactory method names and solve typing error in guard file

--- Backend-dotnet/src/Domain/Validations/Guard.cs
+++ Backend-dotnet/src/Domain/Validations/Guard.cs
@@ -5,17 +5,15 @@ namespace Domain.Validations
     // Check if Timestamp is not null and is in UTC format, throw an exception if it is not
     public static void EnsureUtc(DateTimeOffset value, string propertyName)
     {
-      // Validate that the value is not null
-      if (IsUninitialized(value)) 
+      if (IsUninitialized(value))
         throw new ArgumentException("Timestamp is uninitialized.", propertyName);
 
-      // Validate that the value is in UTC
       if (value.Offset != TimeSpan.Zero)
         throw new ArgumentException("Timestamp must be in UTC (offset 00:00).", propertyName);
     }
 
-    // Check if the string value is null or empty and throw an exception if it is
-    public static void EnsureStringNotEmpty(string value, string propertyName)
+    // Check if the string value is null or whitespace and throw an exception if it is
+    public static void EnsureStringNotNullOrWhiteSpace(string value, string propertyName)
     {
       if (string.IsNullOrWhiteSpace(value))
       {
@@ -23,21 +21,21 @@ namespace Domain.Validations
       }
     }
 
-    // Check if the char value is empty and throw an exception if it is
+    // Check if the char value is uninitialized or it has whitespace and throw an exception if it is
     public static void EnsureCharInitializedAndNotWhiteSpace(char value, string propertyName)
     {
-      if (char.IsWhiteSpace(value))
-      {
-        throw new ArgumentException($"The property '{propertyName}' cannot be whitespace.", propertyName);
-      }
-
       if (IsUninitialized(value))
       {
         throw new ArgumentException($"The property '{propertyName}' is uninitialized.", propertyName);
       }
+
+      if (char.IsWhiteSpace(value))
+      {
+        throw new ArgumentException($"The property '{propertyName}' cannot be whitespace.", propertyName);
+      }
     }
 
-    // Check if the value is default and throw false if it is
+    // Check if the value is default and return true if it is
     private static bool IsUninitialized<T>(T value) => EqualityComparer<T>.Default.Equals(value, default);
   }
 }
