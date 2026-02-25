580366261cc9696f4dea21a8fa9d262a9502d456
Briangel Abreu <briangel29@gmail.com>
2026-02-24 22:10:44 -0400

    feat: add unit test of UserRoles entity

---
+++ Backend-dotnet/test/Domain.Tests/Entities/UserRolesTest.cs
@@ -0,0 +1,87 @@
+using Domain.Entities;
+
+namespace Domain.Tests.Entities;
+
+[Trait("Layer", "Domain")]
+public class UserRolesTest
+{
+    [Fact]
+    [Trait("UserRoles", "Create")]
+    public void Create_ShouldCreateUserRole_WhenParametersAreValid()
+    {
+        // Arrange
+        var userId = Ulid.NewUlid();
+        var roleId = Ulid.NewUlid();
+        var timestamp = DateTimeOffset.UtcNow;
+
+        // Act
+        var userRole = UserRoles.Create(userId, roleId, timestamp);
+
+        // Assert
+        Assert.NotNull(userRole);
+        Assert.Equal(userId, userRole.UserId);
+        Assert.Equal(roleId, userRole.RoleId);
+        Assert.Equal(timestamp, userRole.CreatedAt);
+    }
+
+    [Fact]
+    [Trait("UserRoles", "Create")]
+    public void Create_ShouldThrowException_WhenUserIdIsEmpty()
+    {
+        // Arrange
+        var userId = Ulid.Empty;
+        var roleId = Ulid.NewUlid();
+        var timestamp = DateTimeOffset.UtcNow;
+
+        // Act and Assert
+        var exception = Assert.Throws<ArgumentException>(() => UserRoles.Create(userId, roleId, timestamp));
+
+        Assert.Contains("Is Uninitialized", exception.Message, StringComparison.OrdinalIgnoreCase);
+    }
+
+    [Fact]
+    [Trait("UserRoles", "Create")]
+    public void Create_ShouldThrowException_WhenRoleIdIsEmpty()
+    {
+        // Arrange
+        var userId = Ulid.NewUlid();
+        var roleId = Ulid.Empty;
+        var timestamp = DateTimeOffset.UtcNow;
+
+        // Act and Assert
+        var exception = Assert.Throws<ArgumentException>(() => UserRoles.Create(userId, roleId, timestamp));
+
+        Assert.Contains("Is Uninitialized", exception.Message, StringComparison.OrdinalIgnoreCase);
+    }
+
+    [Fact]
+    [Trait("UserRoles", "Create")]
+    public void Create_ShouldThrowException_WhenTimestampIsUninitialized()
+    {
+        // Arrange
+        var userId = Ulid.NewUlid();
+        var roleId = Ulid.NewUlid();
+        var timestamp = default(DateTimeOffset);
+
+        // Act and Assert
+        var exception = Assert.Throws<ArgumentException>(() => UserRoles.Create(userId, roleId, timestamp));
+
+        Assert.Contains("Is Uninitialized", exception.Message, StringComparison.OrdinalIgnoreCase);
+    }
+
+    [Fact]
+    [Trait("UserRoles", "Create")]
+    public void Create_ShouldThrowException_WhenTimestampIsNotUtc()
+    {
+        // Arrange
+        var userId = Ulid.NewUlid();
+        var roleId = Ulid.NewUlid();
+        var timestamp = DateTimeOffset.Now;
+
+        // Act and Assert
+        var exception = Assert.Throws<ArgumentException>(() => UserRoles.Create(userId, roleId, timestamp));
+
+        Assert.Contains("Must be in UTC", exception.Message, StringComparison.OrdinalIgnoreCase);
+    }
+    
+}
