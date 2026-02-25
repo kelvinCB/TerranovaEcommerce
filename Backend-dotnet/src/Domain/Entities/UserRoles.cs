709fb483be471e38cad57b5b8c4264406eeb8c27
Briangel Abreu <briangel29@gmail.com>
2026-02-24 22:04:42 -0400

    feat: add UserRoles.cs entity in domain layer

---
+++ Backend-dotnet/src/Domain/Entities/UserRoles.cs
@@ -0,0 +1,48 @@
+using Domain.Validations;
+
+namespace Domain.Entities;
+
+/// <summary>
+/// Represents a user-role assignment.
+/// </summary>
+public class UserRoles
+{
+    public Ulid UserId { get; private set; }
+    public Ulid RoleId { get; private set; }
+    public DateTimeOffset CreatedAt { get; private set; }
+
+    /// <summary>
+    /// Initializes a new instance of the UserRole class.
+    /// </summary>
+    /// <param name="userId">The user identifier.</param>
+    /// <param name="roleId">The role identifier.</param>
+    /// <param name="createdAt">The timestamp when the user-role assignment was created.</param>
+    private UserRoles(Ulid userId, Ulid roleId, DateTimeOffset createdAt)
+    {
+        UserId = userId;
+        RoleId = roleId;
+        CreatedAt = createdAt;
+    }
+
+    /// <summary>
+    /// Creates a new instance of the UserRole class and performs necessary validations.
+    /// </summary>
+    /// <param name="userId">The user identifier.</param>
+    /// <param name="roleId">The role identifier.</param>
+    /// <param name="timestamp">The timestamp when the user-role assignment was created.</param>
+    /// <returns>A new instance of the UserRole class.</returns>
+    public static UserRoles Create(Ulid userId, Ulid roleId, DateTimeOffset timestamp)
+    {
+        // Perform validations using the Guard class to ensure domain invariants are maintained
+        Guard.EnsureUlidNotEmpty(userId, nameof(userId));
+        Guard.EnsureUlidNotEmpty(roleId, nameof(roleId));
+        Guard.EnsureUtc(timestamp, nameof(timestamp));
+
+        return new UserRoles(
+            userId,
+            roleId,
+            timestamp
+        );
+    }
+    
+}
