a35f0e1ca738111d6da51cc4e0ed5b60108ef648
Briangel Abreu <briangel29@gmail.com>
2026-03-01 20:56:15 -0400

    test: add unit test of GetUserByIdQueryHanddler.cs

---
+++ Backend-dotnet/test/Application.Tests/Users/Queries/GetUserById/GetUserByIdQueryHandlerTests.cs
@@ -0,0 +1,106 @@
+using Application.Users.Queries.GetUserById;
+using Application.Common.Abstractions.Persistence;
+using Moq;
+
+namespace Application.Tests.Users.Queries.GetUserById;
+
+[Trait("Layer", "Application")]
+public sealed class GetUserByIdQueryHandlerTests
+{
+    [Fact]
+    [Trait("Users", "GetUserByIdQueryHandler")]
+    public void Constructor_ShouldInitialize_WithValidRepository()
+    {
+        // Act and Assert
+        var excepcion = Record.Exception(() => new GetUserByIdQueryHandler(Mock.Of<IUserRepository>()));
+
+        Assert.Null(excepcion);
+    }
+
+    [Fact]
+    [Trait("Users", "GetUserByIdQueryHandler")]
+    public void Constructor_ShouldThrowException_WhenUserRepositoryIsNull()
+    {
+        // Act and Assert
+        var excepcion = Assert.Throws<ArgumentNullException>(() => new GetUserByIdQueryHandler(null!));
+    }
+
+    [Fact]
+    [Trait("Users", "GetUserByIdQueryHandler")]
+    public async Task Handler_ShouldReturnUser_WhenUserExists()
+    {
+        // Arrange
+        var user = Domain.Entities.User.Create(
+            Ulid.NewUlid(),
+            "Briangel",
+            "Santana Calcanio",
+            new DateOnly(2001, 1, 1),
+            'M',
+            Domain.ValueObjects.PasswordHash.From(new String('a', 64)),
+            new DateTimeOffset(2026, 1, 1, 0, 0, 0, TimeSpan.Zero),
+            Domain.ValueObjects.Email.Create("test@example.com"),
+            Domain.ValueObjects.PhoneNumber.Create("+18298881212")
+        );
+
+        var mockUserRepository = new Mock<IUserRepository>();
+
+        mockUserRepository
+            .Setup(x => x.GetByIdAsync(It.IsAny<Ulid>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(user);
+
+        var handler = new GetUserByIdQueryHandler(mockUserRepository.Object);
+
+        // Act
+        var result = await handler.Handle(new GetUserByIdQuery(user.Id), CancellationToken.None);
+
+        // Assert
+        mockUserRepository.Verify(x => x.GetByIdAsync(user.Id, CancellationToken.None), Times.Once);
+
+        Assert.NotNull(result);
+        Assert.Equal(user.Id, result.Id);
+        Assert.Equal(user.FirstName, result.FirstName);
+        Assert.Equal(user.LastName, result.LastName);
+        Assert.Equal(user.BirthDate, result.BirthDate);
+        Assert.Equal(user.Gender, result.Gender);
+        Assert.Equal(user.IsActive, result.IsActive);
+        Assert.Equal(user.CreatedAt, result.CreatedAt);
+        Assert.Equal(user.UpdatedAt, result.UpdatedAt);
+        Assert.Equal(user.EmailAddress.Value, result.EmailAddress);
+        Assert.Equal(user.IsDeleted, result.IsDeleted);
+        Assert.Equal(user.PhoneNumber?.Value, result.PhoneNumber);
+    }
+
+    [Fact]
+    [Trait("Users", "GetUserByIdQueryHandler")]
+    public async Task Handler_ShouldReturnNull_WhenUserDoesNotExist()
+    {
+        // Arrange
+        var user = Domain.Entities.User.Create(
+            Ulid.NewUlid(),
+            "Briangel",
+            "Santana Calcanio",
+            new DateOnly(2001, 1, 1),
+            'M',
+            Domain.ValueObjects.PasswordHash.From(new String('a', 64)),
+            new DateTimeOffset(2026, 1, 1, 0, 0, 0, TimeSpan.Zero),
+            Domain.ValueObjects.Email.Create("test@example.com"),
+            Domain.ValueObjects.PhoneNumber.Create("+18298881212")
+        );
+
+        var mockUserRepository = new Mock<IUserRepository>();
+
+        mockUserRepository
+            .Setup(x => x.GetByIdAsync(It.IsAny<Ulid>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync((Domain.Entities.User?)null);
+
+        var handler = new GetUserByIdQueryHandler(mockUserRepository.Object);
+
+        // Act
+        var result = await handler.Handle(new GetUserByIdQuery(user.Id), CancellationToken.None);
+
+        // Assert
+        mockUserRepository.Verify(x => x.GetByIdAsync(It.IsAny<Ulid>(), It.IsAny<CancellationToken>()), Times.Once);
+        Assert.Null(result);
+    }
+
+}
