d7e335b1c9bdd975397ad8cb9cab447075b3836f
Briangel Abreu <briangel29@gmail.com>
2026-03-17 23:51:23 -0400

    refact: include roles to RegisterUser use case and refact it to return user id

--- Backend-dotnet/src/Application/Users/Commands/RegisterUser/RegisterUserCommandHandler.cs
+++ Backend-dotnet/src/Application/Users/Commands/RegisterUser/RegisterUserCommandHandler.cs
@@ -9,13 +9,14 @@ using MediatR;
 namespace Application.Users.Commands.RegisterUser;
 
 /// <summary>
-/// Represents a command handler for registering a new user and returning the registered user.
+/// Represents a command handler for registering a new user and returning the user ID.
 /// </summary>
-/// <remarks>Mediator pattern is used to handle the command and return the registered user.</remarks>
-public sealed class RegisterUserCommandHandler : IRequestHandler<RegisterUserCommand, UserDto>
+/// <remarks>Mediator pattern is used to handle the command and return the user ID.</remarks>
+public sealed class RegisterUserCommandHandler : IRequestHandler<RegisterUserCommand, Ulid>
 {
     // Dependencies
     private readonly IUserRepository _userRepository;
+    private readonly IRoleRepository _roleRepository;
     private readonly IPasswordHasher _passwordHasher;
     private readonly IDateTimeProvider _dateTimeProvider;
     private readonly IIdGenerator _idGenerator;
@@ -27,14 +28,18 @@ public sealed class RegisterUserCommandHandler : IRequestHandler<RegisterUserCom
     /// <param name="passwordHasher">The password hasher service.</param>
     /// <param name="dateTimeProvider">The date-time provider service.</param>
     /// <param name="idGenerator">The ID generator service.</param>
+    /// <param name="roleRepository">The role repository.</param>
+    /// <exception cref="ArgumentNullException">Thrown when the user repository, role repository, password hasher, date-time provider or ID generator is null.</exception>
     public RegisterUserCommandHandler(
         IUserRepository userRepository,
+        IRoleRepository roleRepository,
         IPasswordHasher passwordHasher,
         IDateTimeProvider dateTimeProvider,
         IIdGenerator idGenerator
     )
     {
         _userRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
+        _roleRepository = roleRepository ?? throw new ArgumentNullException(nameof(roleRepository));
         _passwordHasher = passwordHasher ?? throw new ArgumentNullException(nameof(passwordHasher));
         _dateTimeProvider = dateTimeProvider ?? throw new ArgumentNullException(nameof(dateTimeProvider));
         _idGenerator = idGenerator ?? throw new ArgumentNullException(nameof(idGenerator));
@@ -45,9 +50,10 @@ public sealed class RegisterUserCommandHandler : IRequestHandler<RegisterUserCom
     /// </summary>
     /// <param name="request">The command request.</param>
     /// <param name="cancellationToken">The cancellation token.</param>
-    /// <returns>Returns the registered user.</returns>
+    /// <returns>Returns the user ID.</returns>
     /// <exception cref="EmailAlreadyInUseException">Thrown when the specified email is already in use.</exception>
-    public async Task<UserDto> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
+    /// <exception cref="RoleNotFoundException">Thrown when the specified role ID is not found.</exception>
+    public async Task<Ulid> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
     {
         var email = Email.Create(request.Email);
 
@@ -83,8 +89,19 @@ public sealed class RegisterUserCommandHandler : IRequestHandler<RegisterUserCom
             phoneNumber: phoneNumber
         );
 
+        foreach(var roleId in request.RoleIds)
+        {
+            var roleExists = await _roleRepository.ExistsByIdAsync(roleId, cancellationToken);
+            if (!roleExists)
+            {
+                throw new RoleNotFoundException(roleId);
+            }
+
+            user.AssignRole(roleId, timestamp);
+        }
+
         await _userRepository.RegisterAsync(user, cancellationToken);
 
-        return UserDto.FromDomain(user);
+        return user.Id;
     }
 }
