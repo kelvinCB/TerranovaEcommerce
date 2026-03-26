162add7063c6572701a8fba6fcdd1d15bfa7a441
briangel29 <briangel29@gmail.com>
2026-03-26 08:12:26 -0400

    chore: add XML documentation of IUnitOfWork.cs

--- Backend-dotnet/src/Application/Common/Abstractions/Persistence/IUnitOfWork.cs
+++ Backend-dotnet/src/Application/Common/Abstractions/Persistence/IUnitOfWork.cs
@@ -1,6 +1,13 @@
 namespace Application.Common.Abstractions.Persistence;
 
+/// <summary>
+/// Represents a unit of work for database operations.
+/// </summary>
 public interface IUnitOfWork
 {
+    /// <summary>
+    /// Saves changes to the database.
+    /// </summary>
+    /// <param name="cancellationToken">A cancellation token.</param>
     Task SaveChangesAsync(CancellationToken cancellationToken = default);
 }
