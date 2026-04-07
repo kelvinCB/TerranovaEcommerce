86b5303c60762b614db24916b1a585f63ddafba3
Briangel Abreu <briangel29@gmail.com>
2026-04-07 10:21:28 -0400

    chore: delete application common exception RefreshTokenNotFoundException.cs

--- Backend-dotnet/src/Application/Common/Exceptions/RefreshTokenNotFoundException.cs
+++
@@ -1,6 +0,0 @@
-namespace Application.Common.Exceptions;
-
-public sealed class RefreshTokenNotFoundException : ApplicationExceptionBase
-{
-    public RefreshTokenNotFoundException() : base($"Refresh token was not found.") { }
-}
