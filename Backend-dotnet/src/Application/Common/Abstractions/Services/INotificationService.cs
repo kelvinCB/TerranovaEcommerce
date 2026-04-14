e8c893d4a78873be48e36aef45662187b82223d9
Briangel Abreu <briangel29@gmail.com>
2026-04-13 23:36:22 -0400

    feat: add INotificationServices.cs for sending verification code to email

---
+++ Backend-dotnet/src/Application/Common/Abstractions/Services/INotificationService.cs
@@ -0,0 +1,16 @@
+namespace Application.Common.Abstractions.Services;
+
+/// <summary>
+/// Defines an interface for notification services, providing methods to send various types of notifications to users.
+/// </summary>
+public interface INotificationService
+{
+    /// <summary>
+    /// Sends a password reset code to the specified email address, allowing the user to reset their password.
+    /// </summary>
+    /// <param name="email">The email address to send the password reset code to.</param>
+    /// <param name="code">The password reset code to send.</param>
+    /// <param name="cancellationToken">A cancellation token for the asynchronous operation.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    Task SendPasswordResetCodeToEmailAsync(string email, string code, CancellationToken cancellationToken);
+}
