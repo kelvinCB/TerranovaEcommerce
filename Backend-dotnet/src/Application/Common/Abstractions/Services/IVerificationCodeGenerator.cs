d559a63f298a930d1bcd71104067196d70a4822f
Briangel Abreu <briangel29@gmail.com>
2026-04-13 23:38:05 -0400

    chore: catch verification code generated in a record named GeneratedVerificationCode.cs

--- Backend-dotnet/src/Application/Common/Abstractions/Services/IVerificationCodeGenerator.cs
+++ Backend-dotnet/src/Application/Common/Abstractions/Services/IVerificationCodeGenerator.cs
@@ -1,15 +1,16 @@
-using Domain.ValueObjects;
+using Application.Common.Verification;
 
 namespace Application.Common.Abstractions.Services;
 
 /// <summary>
-/// Defines an interface for generating verification codes used in user verification processes such as password resets or email confirmations.
+/// Defines an interface for generating verification codes used
+/// in user verification processes such as password resets or email confirmations.
 /// </summary>
 public interface IVerificationCodeGenerator
 {
     /// <summary>
-    /// Generates a new verification code according to the defined rules and requirements (e.g., length, character set).
+    /// Generates a new verification code.
     /// </summary>
     /// <returns>Returns the generated verification code.</returns>
-    Code Generate();
+    GeneratedVerificationCode Generate();
 }
