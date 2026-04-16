e56bcc0a9f74eae21e4afbc8d8af0e877f544ac9
Briangel Abreu <briangel29@gmail.com>
2026-04-15 23:38:31 -0400

    feat: add method to send email verification code to email in INotificationService.cs

--- Backend-dotnet/src/Application/Common/Abstractions/Services/INotificationService.cs
+++ Backend-dotnet/src/Application/Common/Abstractions/Services/INotificationService.cs
@@ -13,4 +13,13 @@ public interface INotificationService
     /// <param name="cancellationToken">A cancellation token for the asynchronous operation.</param>
     /// <returns>A task representing the asynchronous operation.</returns>
     Task SendPasswordResetCodeToEmailAsync(string email, string code, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Sends an email verification code to the specified email address, allowing the user to verify their email address.
+    /// </summary>
+    /// <param name="email">The email address to send the verification code to.</param>
+    /// <param name="code">The email verification code to send.</param>
+    /// <param name="cancellationToken">A cancellation token for the asynchronous operation.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    Task SendEmailVerificationCodeToEmailAsync(string email, string code, CancellationToken cancellationToken);
 }
